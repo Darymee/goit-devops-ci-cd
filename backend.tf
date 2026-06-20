ea0b4a8ffed0984c574864625017d5dde5ccb603
Darymee <danieledaniele3005@gmail.com>
2026-06-20 19:02:21 +0200

    Revert "add hw 5"
    This reverts commit 67034bcbdb14bc541f1124801fdcbeb2df723133.

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "danieledaniele3005-lesson-5-tfstate"
-    key            = "lesson-5/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
